a336a07b010ead1ed69f9a36f075c0db4baac9b8
FilovetsAlexandr <sohan.boxan@gmail.com>
2023-09-06 13:29:30 +0300

    project created

---
+++ ChangeBG/ChangeBG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChangeBG
+//
+//  Created by Alexandr Filovets on 6.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
